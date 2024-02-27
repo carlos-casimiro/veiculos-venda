70601f9ed46f52c580cb299a0968f1a0c685f74e
--help <carloscasimirocc@gmail.com>
2024-02-27 23:17:41 +0100

    Arquivo Dashboard_veiculo adicionado

---
+++ dashboard_veiculo.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv(r'C:\Users\carlo\OneDrive\Documentos\Analise de Dados\SPRINT 5\projecto\vehicles.csv')
+
+st.header('DASHBOARD -VEICHLES')
+hist_button = st.button('criar histograma')
+scatter_button=st.button('criar scatter')
+
+
+if hist_button:
+    st.write('criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig =px.histogram(car_data, x='odometer')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button:
+    st.write ('criando um scatter para o conjunto de dados de anúncios de vendas de carros')
+    figx = px.scatter(x='price', y='model_year')
